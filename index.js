2221c231624241fcee944bbf67ac7fa894a5758b
ianpetzer <ianpetzer@gmail.com>
2016-09-19 11:56:28 +0200

    Don't import tether via bower

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    this.app.import('bower_components/tether-shepherd/dist/css/shepherd-theme-arrows.css');
-    this.app.import('bower_components/tether/dist/js/tether.js');
-    this.app.import('bower_components/tether-shepherd/dist/js/shepherd.js');
+    // this.app.import('bower_components/tether-shepherd/dist/css/shepherd-theme-arrows.css');
+    // this.app.import('bower_components/tether/dist/js/tether.js');
+    // this.app.import('bower_components/tether-shepherd/dist/js/shepherd.js');
 
     if (!process.env.EMBER_CLI_FASTBOOT) {
       this.app.import('vendor/jquery-disablescroll/jquery.disablescroll.js');
